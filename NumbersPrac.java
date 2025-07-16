c972bcd545c4907cab10b920c192affcf5012f82
Shrinidhi444 <shrinidhipandit02@gmail.com>
2025-07-16 11:38:06 +0530

    Hello

--- NumbersPrac.java
+++ NumbersPrac.java
@@ -127,24 +127,101 @@ public class NumbersPrac {
 	
 	//Kaprekar no
 	
-	int square = n * n;
-    int temp = square;
-    int digits = 0;
-
-    int num = n;
-    while (n > 0) {
-        digits++;
-        n /= 10;
-    }
-
-    int divisor = (int) Math.pow(10, digits);
-    int rightPart = square % divisor;
-    int leftPart = square / divisor;
-
-    if (leftPart + rightPart == num) {
-        System.out.println(num + " is a Kaprekar Number!");
-    } else {
-        System.out.println(num + " is NOT a Kaprekar Number.");
-    }
+//	int square = n * n;
+//    int temp = square;
+//    int digits = 0;
+//
+//    int num = n;
+//    while (n > 0) {
+//        digits++;
+//        n /= 10;
+//    }
+//
+//    int divisor = (int) Math.pow(10, digits);
+//    int rightPart = square % divisor;
+//    int leftPart = square / divisor;
+//
+//    if (leftPart + rightPart == num) {
+//        System.out.println(num + " is a Kaprekar Number!");
+//    } else {
+//        System.out.println(num + " is NOT a Kaprekar Number.");
+//    }
+	
+	//Duck no
+//	int num = n;
+//	if (num.charAt(0) == '0') {
+//	    System.out.println("Not a Duck number!!");
+//	} else if (num.contains("0")) {
+//	    System.out.println("Duck number!!");
+//	} else {
+//	    System.out.println("Not a Duck number!!");
+//	}
+	
+	//Emirp no
+	
+//	 if (isPrime(n)) {
+//         int rev = reverse(n);
+//         if (rev != n && isPrime(rev)) {
+//             System.out.println(n + " is an Emirp Number!");
+//         } else {
+//             System.out.println(n + " is NOT an Emirp Number.");
+//         }
+//     } else {
+//         System.out.println(n + " is NOT an Emirp Number.");
+//     }
+// }
+//
+// static boolean isPrime(int n) {
+//     if (n <= 1) return false;
+//     for (int i = 2; i <= n / 2; i++) {
+//         if (n % i == 0) return false;
+//     }
+//     return true;
+// }
+//
+// static int reverse(int n) {
+//     int rev = 0;
+//     while (n > 0) {
+//         rev = rev * 10 + n % 10;
+//         n /= 10;
+//     }
+//     return rev;
+//    
+	
+	//Disarium no
+//	 int length = String.valueOf(n).length();
+//
+//     int temp = n;
+//     int sum = 0;
+//
+//     while (temp > 0) {
+//         int digit = temp % 10;
+//         sum += Math.pow(digit, length);
+//         temp /= 10;
+//         length--;
+//     }
+//
+//     if (sum == n) {
+//         System.out.println(n + " is a Disarium Number!");
+//     } else {
+//         System.out.println(n + " is NOT a Disarium Number.");
+//     }
+	
+	//Healthy or deficient
+	int sum=0;
+	for(int i=1;i<n;i++) {
+		if(n%i==0) {
+			sum += i;
+		}
+	}
+	if(sum < n) {
+		System.out.print("Deffective!!");
+	}else if(sum>n){
+		System.out.print("Healthy!!");
+	}else {
+		System.out.print("Perfect!!");
+	}
+	
+	
 	}
 }
---
+++ Operationss.java
@@ -0,0 +1,37 @@
+package PacJava;
+
+public class Operationss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		for(int i = 0;i <= 10;System.out.println(" "+(++i))) {			
+//		}
+//		System.out.println();
+//		
+//		for(int i = 0;i <= 10;System.out.println(" "+(i++))) {			
+//		}
+		
+		if(System.out.printf("Hello") == null) {
+			System.out.println("Hello World!!!");
+		}
+		else {
+			System.out.println("World!!!");
+		}
+	}
+	
+}
+		
+		//C
+//		int main() {
+//			int a = printf("Hello World");
+//			printf(""%d",a);
+//		if(!printf("Hello")) {
+//			printf("Hello");
+//		}
+//		else {
+//			printf("World");
+//		}
+//		return 0;
+//	}
+
+
