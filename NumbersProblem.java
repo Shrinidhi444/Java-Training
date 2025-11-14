3b7efe230f120360c9a9def4138cf6bd65ec4e71
Shrinidhi444 <shrinidhipandit02@gmail.com>
2025-11-14 10:47:26 +0530

    Number Problems

--- NumbersProblem.java
+++ NumbersProblem.java
@@ -22,14 +22,33 @@ public class NumbersProblem {
 		}
 	}
 
+	static void SumofN(int n) {
+		int sum=n*(n+1)/2;
+		for(int i=1;i<=n;i++) {
+			sum += i;
+		}
+		System.out.println("The Sum of "+n+" is: "+sum);		
+	}
+	
+	static void LeapYear(int y) {
+		if(y%4==0 || (y%400==0 && y%100!=0)) {
+			System.out.println("Leap");
+		}else {
+			System.out.println("Not leap");
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		System.out.print("Enter number:");
 		int num=sc.nextInt();
+		System.out.print("Enter Year");
+		int Year=sc.nextInt();
 		
 		PN(num);
 		EO(num);
+		SumofN(num);
+		LeapYear(Year);
 	
 	}
 
